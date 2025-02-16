71c2ec8c223631847d1e5202f5165345f6c29aa6
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-02-16 22:39:57 +0700

    add: basic routing, controller, and view based on requirements

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,44 @@
 <?php
 
+use App\Http\Controllers\POSController;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * Home page
+ */
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * The implementation of route prefix
+ * Product pages
+ */
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', function () {
+        return "Food beverage";
+    });
+    Route::get('/beauty-health', function () {
+        return "Beauty health";
+    });
+    Route::get('/home-care', function () {
+        return "Home care!";
+    });
+    Route::get('/baby-kid', function () {
+        return "Baby kid";
+    });
+});
+
+/**
+ * The implementation of route params
+ * User pages
+ */
+Route::get('/user/{id}/name/{name}', function ($id, $name) {
+    return "Hello $name! You have ID $id isn't it ?";
+});
+
+
+/**
+ * Transaction pages
+ */
+Route::resource('pos', POSController::class);
